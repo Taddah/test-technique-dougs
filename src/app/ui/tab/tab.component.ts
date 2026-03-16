fa5895b07ed44833c632d0f72eaabb7a3fa91daf
Thomas <thomas.bernard72@gmail.com>
2026-03-16 13:01:16 +0100

    feat: add tab component and its stories

---
+++ src/app/ui/tab/tab.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-tab',
+  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  template: `
+    <a class="tab tab-text" [routerLink]="[link()]" routerLinkActive="tab__active">
+      <ng-content></ng-content>
+    </a>
+  `,
+  styles: `
+    .tab {
+      display: inline-flex;
+      align-items: center;
+      gap: 8px;
+      padding: 8px;
+      color: black;
+      text-decoration: none;
+
+      &:focus {
+        cursor: pointer;
+      }
+
+      &__active {
+        color: var(--cerulean) !important;
+        background-color: var(--light-blue);
+      }
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Tab {
+  link = input<string>('');
+}
