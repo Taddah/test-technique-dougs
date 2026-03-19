bcd5c8e2d30f5415c5ed8a322287d640bc0eccd2
Thomas <thomas.bernard72@gmail.com>
2026-03-19 23:38:00 +0100

    chore: add proxy.config and new start-with-proxy command

--- src/app/core/tokens/category-api-url.token.ts
+++ src/app/core/tokens/category-api-url.token.ts
@@ -1,8 +1,8 @@
 import { InjectionToken } from '@angular/core';
 import { environment } from '@environment/environment';
 
-// Token pour l'URL de base de l'API
 export const CATEGORY_API_URL = new InjectionToken<string>(environment.categoryApiUrl, {
   providedIn: 'root',
-  factory: () => 'http://localhost:3001',
+  factory: () => '/api',
+  //factory: () => 'http://localhost:3001',
 });
